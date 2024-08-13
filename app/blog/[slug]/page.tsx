e455dd37dfc5b5771cfd99d4952e404bbf7dc318
Le Nguyen Khoi <mariokhoi2028@gmail.com>
2024-08-13 14:08:19 +0700

    clear

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: Params) {
 
 export default function Page({ params }: Params) {
   return <><h1>Slug: {params.slug}</h1>
-  <p>Hole Tex</p>
+  <p>Hole Texxx</p>
   <p>Welcome to HCM</p>
   </>;
 }
